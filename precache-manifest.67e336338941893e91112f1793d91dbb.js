a9fa0a9e6ad0bbad01174d8b04860d17addecacd
Elena Santana <fanelenan7@gmail.com>
2019-04-19 13:38:22 -0400

    Updates

---
+++ precache-manifest.67e336338941893e91112f1793d91dbb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b177861f6428055ee07c00172e4ec1c0",
+    "url": "/static/media/tower-of-hanoi.b177861f.jpg"
+  },
+  {
+    "revision": "ac9e3db3a71fed2d870c",
+    "url": "/static/css/main.d5982bce.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d2c365098599dc28d812",
+    "url": "/static/js/2.fd89c939.chunk.js"
+  },
+  {
+    "revision": "c0ec9812bf35f0687f7bb0ae3c7f21df",
+    "url": "/static/media/portrait.c0ec9812.jpg"
+  },
+  {
+    "revision": "f56690765e5ea9d7bb5578ed0a17918f",
+    "url": "/static/media/ibmwh.f5669076.png"
+  },
+  {
+    "revision": "c84c5ab8b4807d19c1305c4a026df2da",
+    "url": "/static/media/av&c.c84c5ab8.jpeg"
+  },
+  {
+    "revision": "87bd52c8fb86573b170d91143d3f4179",
+    "url": "/static/media/coder-ipsum.87bd52c8.jpg"
+  },
+  {
+    "revision": "a99fe59233c4f5cb9cda43d26e1f5f56",
+    "url": "/static/media/people-finder.a99fe592.jpg"
+  },
+  {
+    "revision": "ac9e3db3a71fed2d870c",
+    "url": "/static/js/main.0f553c2f.chunk.js"
+  },
+  {
+    "revision": "f9839c2de68e9f4884b79fc63e22e789",
+    "url": "/static/media/mariana.f9839c2d.png"
+  },
+  {
+    "revision": "84c12ff953d20c0447d77c36d8e4a474",
+    "url": "/static/media/barrys.84c12ff9.png"
+  },
+  {
+    "revision": "cacd67cc3bff2ca8a0d4031220ff2067",
+    "url": "/static/media/brrrn.cacd67cc.png"
+  },
+  {
+    "revision": "0e86d68bbb9a560baa4b38faa98b25d0",
+    "url": "/static/media/bikerbarre.0e86d68b.jpg"
+  },
+  {
+    "revision": "c3bffa7e2808a0ba2ea7335e64523325",
+    "url": "/static/media/sculpthouse.c3bffa7e.png"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "2edbccd89b6168336bed495f19ffb3e1",
+    "url": "/static/media/arrows.2edbccd8.png"
+  },
+  {
+    "revision": "9ff12f694e5951a6f51a9d63b05062e7",
+    "url": "/static/media/OpenSans-Light.9ff12f69.ttf"
+  },
+  {
+    "revision": "b5db0e3a9f38b8cfd28c81ef0ec7b389",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
-"use strict";var precacheConfig=[["/index.html","6d8a9232dedafbc71ea392759a8caaaa"],["/static/css/main.5839c010.css","ca2690488072ed63550081a60ec54923"],["/static/js/main.d1a41587.js","664ecc0ae802052fba83a90ff485b4ba"],["/static/media/OpenSans-Light.9ff12f69.ttf","9ff12f694e5951a6f51a9d63b05062e7"],["/static/media/OpenSans-Regular.d7d5d458.ttf","d7d5d4588a9f50c99264bc12e4892a7c"],["/static/media/arrows.2edbccd8.png","2edbccd89b6168336bed495f19ffb3e1"],["/static/media/av&c.c84c5ab8.jpeg","c84c5ab8b4807d19c1305c4a026df2da"],["/static/media/coder-ipsum.87bd52c8.jpg","87bd52c8fb86573b170d91143d3f4179"],["/static/media/fontawesome-webfont.674f50d2.eot","674f50d287a8c48dc19ba404d20fe713"],["/static/media/fontawesome-webfont.912ec66d.svg","912ec66d7572ff821749319396470bde"],["/static/media/fontawesome-webfont.af7ae505.woff2","af7ae505a9eed503f8b8e6982036873e"],["/static/media/fontawesome-webfont.b06871f2.ttf","b06871f281fee6b241d60582ae9369b9"],["/static/media/fontawesome-webfont.fee66e71.woff","fee66e712a8a08eef5805a46892932ad"],["/static/media/ibmwh.f5669076.png","f56690765e5ea9d7bb5578ed0a17918f"],["/static/media/people-finder.a99fe592.jpg","a99fe59233c4f5cb9cda43d26e1f5f56"],["/static/media/portrait.c0ec9812.jpg","c0ec9812bf35f0687f7bb0ae3c7f21df"],["/static/media/tower-of-hanoi.b177861f.jpg","b177861f6428055ee07c00172e4ec1c0"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var a=new URL(e);return"/"===a.pathname.slice(-1)&&(a.pathname+=t),a.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,a,n){var r=new URL(e);return n&&r.pathname.match(n)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(a)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var a=new URL(t).pathname;return e.some(function(e){return a.match(e)})},stripIgnoredUrlParameters=function(e,a){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return a.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],a=e[1],n=new URL(t,self.location),r=createCacheKey(n,hashParamName,a,/\.\w{8}\./);return[n.toString(),r]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(n){return setOfCachedUrls(n).then(function(a){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!a.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return n.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var a=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!a.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,a=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),n="index.html";(e=urlsToCacheKeys.has(a))||(a=addDirectoryIndex(a,n),e=urlsToCacheKeys.has(a));var r="/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(a=new URL(r,self.location).toString(),e=urlsToCacheKeys.has(a)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(a)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.67e336338941893e91112f1793d91dbb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
